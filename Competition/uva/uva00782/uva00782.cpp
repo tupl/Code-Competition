5bec309178465f404d8fcb22cbcc76cf78c7b31d
Tu Le 261 <thelastdarkman@gmail.com>
2016-02-03 17:42:01 -0800

    fixed dropbox problem

--- Competition/uva/uva00782/uva00782.cpp
+++ Competition/uva/uva00782/uva00782.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <algorithm>
 #include <string>
@@ -10,17 +9,3 @@ using namespace std;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii;
-=======
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <queue>
-#include <vector>
-#include <stack>
-using namespace std;
-
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
->>>>>>> f0c25fcdc79de5684a878bb936788db73b6f8cba
-vector<vii> AdjList;
