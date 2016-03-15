85a41f76ac786d1ef973930385b5aaa5fb22a78b
Tu Le 261 <thelastdarkman@gmail.com>
2016-03-14 20:37:13 -0700

    finished all

---
+++ Competition/uva/uva10557/uva10557.cpp
@@ -0,0 +1,12 @@
+//@keyword sssp, bellman
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+int V, E;
